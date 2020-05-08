ef9c904597434e145c255cec0778318afea88356
Douglas Mason <kayote@koyote.skycastle.enhanceye.com>
2020-05-08 12:15:40 -0700

    set up main code so it can be imported

--- sub_units/utils.py
+++ sub_units/utils.py
@@ -81,8 +81,8 @@ class ConvolutionModel:
 
         if not os.path.exists('state_plots'):
             os.mkdir('state_plots')
-        if not os.path.exists(path.join('state_plots', max_date_str.replace('-', '_')):
-            os.path.exists(path.join('state_plots', max_date_str.replace('-', '_'))
+        if not os.path.exists(path.join('state_plots', max_date_str.replace('-', '_'))):
+            os.mkdir(path.join('state_plots', max_date_str.replace('-', '_')))
         if not os.path.exists(self.plot_filename_base):
             os.mkdir(self.plot_filename_base)
 
