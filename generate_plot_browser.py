dbd3b45e62575b40a2af34242e2612463b377110
Douglas Mason <kayote@koyote.skycastle.enhanceye.com>
2020-05-10 10:35:27 -0700

    Added Full U.S. Report to plot_browser

--- generate_plot_browser.py
+++ generate_plot_browser.py
@@ -1,11 +1,12 @@
 from yattag import Doc
-import sub_units.load_data as load_data 
+import sub_units.load_data as load_data
 from os import path
 import os
 
 base_url_dir = 'https://covid-figures.s3-us-west-2.amazonaws.com/2020_05_06_date_100_bootstraps_100000_likelihood_samples/'
 github_url = 'https://github.com/douglasmason/covid_model'
 plot_browser_dir = 'plot_browser'
+full_report_filename = 'full_us_report.html'
 
 if not path.exists(plot_browser_dir):
     os.mkdir(plot_browser_dir)
@@ -35,7 +36,7 @@ for state in alphabetical_states:
 
     state_lc = state.lower().replace(' ', '_')
     doc, tag, text = Doc(defaults={'title': f'Plots for {state}'}).tagtext()
-    
+
     doc.asis('<!DOCTYPE html>')
     with tag('html'):
         with tag('head'):
@@ -55,10 +56,10 @@ for state in alphabetical_states:
                                 doc.text(figure_name)
                         with tag('hr'):
                             pass
-    
+
     result = doc.getvalue()
     map_state_to_html[state] = result
-    
+
 for state in map_state_to_html:
     state_lc = state.lower().replace(' ', '_')
     if not path.exists(path.join(plot_browser_dir, state_lc)):
@@ -66,8 +67,58 @@ for state in map_state_to_html:
     with open(path.join(plot_browser_dir, path.join(state_lc, 'index.html')), 'w') as f:
         f.write(map_state_to_html[state])
 
+#####
+# Generate state-report page
+#####
+
+
+list_of_figures = [
+'boxplot_for_I_0_with_direct_samples.png',
+'boxplot_for_I_0_without_direct_samples.png',
+'boxplot_for_alpha_1_with_direct_samples.png',
+'boxplot_for_alpha_1_without_direct_samples.png',
+'boxplot_for_alpha_2_with_direct_samples.png',
+'boxplot_for_alpha_2_without_direct_samples.png',
+'boxplot_for_contagious_to_deceased_delay_with_direct_samples.png',
+'boxplot_for_contagious_to_deceased_delay_without_direct_samples.png',
+'boxplot_for_contagious_to_deceased_mult_with_direct_samples.png',
+'boxplot_for_contagious_to_deceased_mult_without_direct_samples.png',
+'boxplot_for_contagious_to_positive_delay_with_direct_samples.png',
+'boxplot_for_contagious_to_positive_delay_without_direct_samples.png',
+'boxplot_for_positive_to_deceased_delay_with_direct_samples.png',
+'boxplot_for_positive_to_deceased_delay_without_direct_samples.png',
+'boxplot_for_positive_to_deceased_mult_with_direct_samples.png',
+'boxplot_for_positive_to_deceased_mult_without_direct_samples.png',
+]
+
+with open(path.join(plot_browser_dir, full_report_filename), 'w') as f:
+    doc, tag, text = Doc(defaults={'title': f'Plots for Full U.S. Report'}).tagtext()
+    doc.asis('<!DOCTYPE html>')
+    with tag('html'):
+        with tag('head'):
+            pass
+        with tag('body'):
+            with tag('div', id='photo-container'):
+                with tag('ul'):
+                    with tag('li'):
+                        with tag('a', href='index.html'):
+                            text('<-- Back')
+                    for figure_name in list_of_figures:
+                        tmp_url = base_url_dir + figure_name
+                        with tag("a", href=tmp_url):
+                            doc.stag('img', src=tmp_url, klass="photo", height="400", width="300")
+                        with tag('li'):
+                            with doc.tag("a", href=tmp_url):
+                                doc.text(figure_name)
+                        with tag('hr'):
+                            pass
+    f.write(doc.getvalue())
+
+#####
+# Generate landing page
+#####
+
 with open(path.join(plot_browser_dir, 'index.html'), 'w') as f:
-    
     doc, tag, text = Doc(defaults={'title': f'Plots for {state}'}).tagtext()
 
     doc.asis('<!DOCTYPE html>')
@@ -78,7 +129,10 @@ with open(path.join(plot_browser_dir, 'index.html'), 'w') as f:
             with tag('ul'):
                 with tag('li'):
                     with tag("a", href=github_url):
-                        text('<-- Back to repository') 
+                        text('<-- Back to Repository')
+                with tag('li'):
+                    with tag("a", href=full_report_filename):
+                        text('Full U.S. Report')
                 for state in alphabetical_states:
                     state_lc = state.lower().replace(' ', '_')
                     tmp_url = state_lc + '/index.html'
